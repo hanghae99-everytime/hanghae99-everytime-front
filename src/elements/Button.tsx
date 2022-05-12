e4497fdea6d54f067af446e6c556473e46df703c
zinny22 <hyejinsang@naver.com>
2022-05-12 23:44:25 +0900

    :lipstick: button element & bg 기본틀 작업 완료

--- src/elements/Button.tsx
+++ src/elements/Button.tsx
@@ -1,9 +1,65 @@
 import React from "react";
+import styled from 'styled-components';
 
-interface ButtonProps {}
+interface ButtonProps {
+  text: boolean
+  children: any
+  _onClick: () => void
+  margin: string
+  width: string
+  height: string
+  padding: string
+  active: boolean
+  bg: string
+  color: string
+  br: string
+  top: string
+  left: string
+}
 
-const Button: React.FC<ButtonProps> = () => {
-  return <React.Fragment></React.Fragment>;
+const Button = ({ text, _onClick, children, margin, width, padding, height, active, bg, color, br, top, left }: ButtonProps) => {
+  const styled = { margin, width, height, padding, bg, color, br, top, left }
+  return <React.Fragment>
+    <Btn
+      {...styled}
+      onClick={_onClick}
+      {...active}
+    >
+      {children}
+    </Btn>
+  </React.Fragment>;
 };
+Button.defaultProps = {
+  text: false,
+  children: null,
+  _onClick: () => { },
+  margin: '',
+  width: '',
+  height: '',
+  padding: '',
+  active: false,
+  bg: '',
+  color: '',
+  br: '',
+  top: '',
+  left:''
+}
+
+
+const Btn = styled.button`
+  background-color: ${(props: any) => (props.bg==='red' ? '#DF0000' : '#fff')};;
+  border: 0;
+  ${(props: any) => (props.br ? `border-radius:${props.br};` : '')};
+  box-shadow: 0 1px 1px 1px rgba(113, 113, 113, 0.3);
+  color: ${(props:any)=>(props.color==='black'?'#000000':'#fff')};
+  ${(props: any) => (props.width ? `width:${props.width};` : '')};
+  ${(props: any) => (props.margin ? `margin:${props.margin};` : '')};
+  ${(props: any) => (props.height ? `height:${props.height};` : '')};
+  ${(props: any) => (props.padding ? `padding:${props.padding};` : '')};
+  cursor: pointer;
+  position: absolute;
+  ${(props: any) => (props.top ? `top:${props.top};` : '')};
+  ${(props: any) => (props.left ? `left:${props.left};` : '')};
+`
 
 export default Button;
--- src/pages/Detail.tsx
+++ src/pages/Detail.tsx
@@ -4,23 +4,54 @@ import styled from 'styled-components';
 //elements
 import Input from '../elements/Input';
 import Ba from '../elements/Background';
+import Button from "../elements/Button";
+import Dives from '../elements/Dives';
 
 interface DetailProps {}
 
 const Detail: React.FC<DetailProps> = () => {
   const [input, setInput] = useState("")
   return (
-    <>
+    <Wrap>
       <Ba>
-      <Input
-        type="text"
-        placeholder="댓글 작성"
-        _onChange={() => { }}
-        width="400px"
+        <Dives>
+          <div className="title">항해99에 타</div>
+          <Button
+            width="145px"
+            height="26px"
+            br="40px"
+            top='91px'
+            left="21px"
+          >로그인</Button>
+          <Button
+            width="145px"
+            height="26px"
+            br="40px"
+            top='124px'
+            left="21px"
+          >글작성하기</Button>
+          
+        </Dives>
+        <Input
+          type="text"
+          placeholder="댓글 작성"
+          _onChange={() => { }}
+          width="400px"
         />
-        </Ba>
-    </>
+      </Ba>
+    </Wrap>
   )
 };
+
+const Wrap = styled.div`
+  .title{
+    font-size: 24px;
+    font-weight: 700;
+    color: #FB0000;
+    position: absolute;
+    top: 43px;
+    left: 32px;
+  }
+`
 
 export default Detail;
