5f03b98b69503f2d602a8741f9e5b9a6e2857556
zinny22 <hyejinsang@naver.com>
2022-05-16 23:36:01 +0900

    :lipstick: 사이드 작업 완료

--- src/redux/modules/atrticles.js
+++ src/redux/modules/atrticles.js
@@ -1,27 +1,39 @@
 import { createAction, handleActions } from "redux-actions";
 import { produce } from "immer";
+import axios from "axios";
 
 //actions
-const LOG_OUT = "LOG_OUT";
+const TOTAL_ARTICLES_COUNT = "TOTAL_ARTICLES_COUNT";
 
 //action creators
-const logOut = createAction(LOG_OUT, (user) => ({ user }));
+const totalArticlesCount = createAction(TOTAL_ARTICLES_COUNT, (totalArticlesCount) => (totalArticlesCount));
 
 //initialState
-const initialState = {};
+const initialState = {
+  totalArticlesCount: 0
+};
 
 //middleware actions
 
+const getTotalArticles = ()=>{
+  return function (dispatch, getState, { history }) {
+    axios
+      .get()
+  }
+}
+
 export default handleActions(
   {
-    [LOG_OUT]: (state, action) => produce(state, (draft) => {}),
+    [TOTAL_ARTICLES_COUNT]: (state, action) => produce(state, (draft) => {
+      draft.totalArticlesCount = action.payload.totalArticlesCount
+    }),
   },
   initialState
 );
 
 //action creator export
 const actionCreators = {
-  logOut,
+  TOTAL_ARTICLES_COUNT,
 };
 
 export { actionCreators };
